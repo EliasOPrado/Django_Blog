67a92e916790011f52e6e23ef53c83bc74bde9cc
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-02-02 20:26:28 +0000

    added articles to article_list.html template

--- articles/admin.py
+++ articles/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Article
 # Register your models here.
 
-#import classes to admin
+#import classes to admin (to show on admin)
 admin.site.register(Article)
